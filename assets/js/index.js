7bdf33eb90314fe44bda734c5f8ec2d6ef63890d
Maurício <euiciowr@gmail.com>
2020-04-05 20:19:37 -0300

    fix: css media queries, nomalize.css and more bugs

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,61 @@
-!function(){document.getElementById("formulary").addEventListener("submit",function(e){e.preventDefault();let n=e.target.querySelector(".peso"),r=e.target.querySelector(".altura"),u=Number(n.value),o=Number(r.value);if(!u)return void t("Peso não encontrado",!0);if(!o)return void t("Altura não encontrada",!0);const a=function(e,t){return!(!e||!t)&&e/t**2}(u,o).toFixed(2);t(`Seu índice IMC é ${a}, a faixa é: (${function(e){const t=["Abaixo do peso","Peso normal","Sobrepeso","Obesidade grau 1","Obesidade grau 2","Obesidade grau 3"];if(e<18.5)return t[0];if(e>=18.5&&e<=24.9)return t[1];if(e>=25&&e<=29.9)return t[2];if(e>=30&&e<=34.9)return t[3];if(e>=35&&e<=39.9)return t[4];if(e>=40)return t[5]}(a)})`)});const e=()=>document.createElement("p");function t(t,n){const r=document.getElementById("resultado");r.innerHTML="";let u=e();n?u.classList.add("bad"):u.classList.add("successful-result"),u.innerHTML=t,r.appendChild(u)}}();
+(function () {
+  const form = document.getElementById("formulary")
+  form.addEventListener("submit", function (e) {
+    e.preventDefault()
+
+    let getInputPeso = e.target.querySelector(".peso")
+    let getInputAltura = e.target.querySelector(".altura")
+    
+    let peso = Number(getInputPeso.value)
+    let altura = Number(getInputAltura.value)
+
+    
+    if (!peso) {
+      setResult("Peso não encontrado", true);
+      return;
+    }
+    
+    if (!altura) {
+      setResult("Altura não encontrada", true);
+      return;
+    }
+
+    const getIndiceMassaCorporal = getImc(peso, altura).toFixed(2);
+    const levelIndiceMassaCorporal = getLevelImc(getIndiceMassaCorporal);
+
+    const mensagem = `Seu índice IMC é ${getIndiceMassaCorporal}, a faixa é: <b>${levelIndiceMassaCorporal}</b>`
+    setResult(mensagem)
+  })
+
+  function getImc(peso, altura) {
+    return peso && altura ? peso / (altura ** 2) : false;
+  }
+
+  function getLevelImc(levelImcValue) {
+    const levelIMC = ["Abaixo do peso", "Peso normal", "Sobrepeso", "Obesidade grau 1", "Obesidade grau 2", "Obesidade grau 3"]
+    if (levelImcValue < 18.5) return levelIMC[0];
+    if (levelImcValue >= 18.5 && levelImcValue <= 24.9) return levelIMC[1];
+    if (levelImcValue >= 25 && levelImcValue <= 29.9) return levelIMC[2];
+    if (levelImcValue >= 30 && levelImcValue <= 34.9) return levelIMC[3];
+    if (levelImcValue >= 35 && levelImcValue <= 39.9) return levelIMC[4];
+    if (levelImcValue >= 40) return levelIMC[5];
+  }
+
+  const createInnerHTMLElement = () => document.createElement('p');
+
+  function setResult(msg, isValid) {
+    const setUIResult = document.getElementById("resultado")
+    setUIResult.innerHTML = ''
+
+    let getCreateInnerHTMLElement = createInnerHTMLElement();
+
+    if (!isValid) {
+      getCreateInnerHTMLElement.classList.add("successful-result")
+    } else {
+      getCreateInnerHTMLElement.classList.add("bad")
+    }
+
+    getCreateInnerHTMLElement.innerHTML = msg
+    setUIResult.appendChild(getCreateInnerHTMLElement)
+  }
+})()
